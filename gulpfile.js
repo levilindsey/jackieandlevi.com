eb298926902a5dc56f6d178f34d6d36bb69979ec
Levi Lindsey <levi@idean.com>
2014-07-28 09:40:53 -0700

    Adds nodemon as a gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,17 @@
 
 var projectName = 'jackieandlevi',
 
-    SCRIPTS_SRC = 'src/scripts/**/*.js',
-    STYLES_GLOB_SRC = 'src/styles/**/*.scss',
-    STYLES_MAIN_SRC = 'src/styles/main.scss',
-    IMAGES_SRC = 'src/images/**/*',
-    SERVER_TESTS_SRC = 'src/server/tests/**/*_test.js',
-    FRONT_END_TESTS_SRC = 'src/apps/*/public/**/*_test.js',
+    scriptsSrcPath = 'src/scripts/**/*.js',
+    stylesGlobSrcPath = 'src/styles/**/*.scss',
+    stylesMainSrcPath = 'src/styles/main.scss',
+    imagesSrcPath = 'src/images/**/*',
+    serverTestsSrcPath = 'src/server/tests/**/*_test.js',
+    frontEndTestsSrcPath = 'src/apps/*/public/**/*_test.js',
 
-    DIST = 'dist',
-    SCRIPTS_DIST = DIST + '/scripts',
-    STYLES_DIST = DIST + '/styles',
-    IMAGES_DIST = DIST + '/images',
+    distPath = 'distPath',
+    scriptsDistPath = distPath + '/scripts',
+    stylesDistPath = distPath + '/styles',
+    imagesDistPath = distPath + '/images',
 
     sourcemapPath = '../src/scss',
 
@@ -20,51 +20,51 @@ var projectName = 'jackieandlevi',
     plugins = require('gulp-load-plugins')();
 
 gulp.task('scripts', function () {
-  return gulp.src(SCRIPTS_SRC)
+  return gulp.src(scriptsSrcPath)
       .pipe(plugins.concat(projectName + '.js'))
-      .pipe(gulp.dest(SCRIPTS_DIST))
+      .pipe(gulp.dest(scriptsDistPath))
       .pipe(plugins.filesize())
       .pipe(plugins.rename({suffix: '.min'}))
       .pipe(plugins.uglify())
-      .pipe(gulp.dest(SCRIPTS_DIST))
+      .pipe(gulp.dest(scriptsDistPath))
       .pipe(plugins.filesize())
       .pipe(plugins.notify({message: 'scripts task complete'}));
 });
 
 gulp.task('styles', function () {
-  return gulp.src(STYLES_MAIN_SRC)
+  return gulp.src(stylesMainSrcPath)
       // TODO: go back to rubySass if it isn't broken anymore...
       //.pipe(plugins.rubySass({style: 'expanded', sourcemap: true, sourcemapPath: sourcemapPath}))
       .pipe(plugins.sass({sourceComments: 'map'}))
       .pipe(plugins.autoprefixer('last 2 version'))
-      .pipe(gulp.dest(STYLES_DIST))
+      .pipe(gulp.dest(stylesDistPath))
       .pipe(plugins.rename({suffix: '.min'}))
       .pipe(plugins.minifyCss())
-      .pipe(gulp.dest(STYLES_DIST))
+      .pipe(gulp.dest(stylesDistPath))
       .pipe(plugins.notify({message: 'styles task complete'}));
 });
 
 gulp.task('images', function () {
-  return gulp.src(IMAGES_SRC)
+  return gulp.src(imagesSrcPath)
       .pipe(plugins.cache(plugins.imagemin({optimizationLevel: 3, progressive: true, interlaced: true})))
-      .pipe(gulp.dest(IMAGES_DIST))
+      .pipe(gulp.dest(imagesDistPath))
       .pipe(plugins.notify({message: 'images task complete'}));
 });
 
 gulp.task('tests-once', ['server-tests-once', 'front-end-tests-once']);
 
 gulp.task('server-tests-once', function () {
-  return gulp.src(SERVER_TESTS_SRC, {read: false})
+  return gulp.src(serverTestsSrcPath, {read: false})
       .pipe(plugins.mocha({reporter: 'dot', ui: 'tdd'}));
 });
 
 gulp.task('front-end-tests-once', function () {
-  return gulp.src(FRONT_END_TESTS_SRC)
+  return gulp.src(frontEndTestsSrcPath)
       .pipe(plugins.karma({configFile: 'karma.conf.js', action: 'run'}));
 });
 
 gulp.task('front-end-tests-tdd', function () {
-  return gulp.src(FRONT_END_TESTS_SRC)
+  return gulp.src(frontEndTestsSrcPath)
       .pipe(plugins.karma({configFile: 'karma.conf.js', action: 'watch'}));
 });
 
@@ -75,26 +75,37 @@ gulp.task('bump', function () {
 });
 
 gulp.task('clean', function () {
-  return gulp.src(DIST, {read: false})
+  return gulp.src(distPath, {read: false})
       .pipe(plugins.clean());
 });
 
 gulp.task('default', ['clean'], function () {
-  gulp.start('tests-once', 'styles', 'scripts', 'watch');//, 'images');// TODO: add image compression for future, image-dependent projects
+  gulp.start('demon');//, 'images');// TODO: add image compression for future, image-dependent projects
 });
 
 gulp.task('watch', ['front-end-tests-tdd'], function () {
-  var server = plugins.livereload();
+  plugins.livereload.listen();
 
-  gulp.watch(DIST + '/**').on('change', function (file) {
-    server.changed(file.path);
-  });
+  gulp.watch(distPath + '/**').on('change', plugins.livereload.changed);
 
-  gulp.watch(STYLES_GLOB_SRC, ['styles']);
+  gulp.watch(stylesGlobSrcPath, ['styles']);
 
-  gulp.watch(SCRIPTS_SRC, ['scripts']);
+  gulp.watch(scriptsSrcPath, ['scripts']);
 
-//  gulp.watch(IMAGES_SRC, ['images']);// TODO: add image compression for future, image-dependent projects
+//  gulp.watch(imagesSrcPath, ['images']);// TODO: add image compression for future, image-dependent projects
 
-  gulp.watch(SERVER_TESTS_SRC, ['server-tests-once']);
+  gulp.watch(serverTestsSrcPath, ['server-tests-once']);
+});
+
+gulp.task('demon', function () {
+  plugins.nodemon({
+    script: 'src/main.js',
+    ext: 'js json html css jade scss',
+    delay: 1.5
+  })
+      .on('start', ['watch'])
+      .on('change', ['watch'])
+      .on('restart', function () {
+        console.log('Server restarted');
+      });
 });
