a629f3556b40030d798f78d3902d2a9e1accd29f
Levi Lindsey <levisl176@gmail.com>
2014-12-29 19:50:17 -0800

    Tweaks.

--- apps/profiles-hex-grid/routes.js
+++ apps/profiles-hex-grid/routes.js
@@ -1,34 +1,24 @@
 // This module is important for connecting this app to the server. It exports a function, which attaches all of the
 // necessary route handlers for this app.
 
-var jackieRouteRegex = /^\/jackie(?:\/.*)?$/;
 var leviRouteRegex = /^\/levi(?:\/.*)?$/;
-var hexGridRoutRegex = /^\/hex-grid(?:\/.*)?$/;
-var templateFile = '/templates/index';
-
-var templatePath = null;
+var hexGridRouteRegex = /^\/hex-grid(?:\/.*)?$/;
 
 // Attaches the route handlers for this app.
 exports.attachRoutes = function (server, appPath, config) {
-  templatePath = appPath + templateFile;
-
-  //server.get(jackieRouteRegex, function(req, res, next) {
-  //  handleRequest(req, res, next, 'jackie');
-  //});
-  server.get(leviRouteRegex, function(req, res, next) {
-    handleRequest(req, res, next, 'levi');
-  });
-  server.get(hexGridRoutRegex, function(req, res, next) {
-    handleRequest(req, res, next, 'levi');
-  });
+  server.get(leviRouteRegex, handleRequest);
+  server.get(hexGridRouteRegex, handleRequest);
 
   // ---  --- //
 
   // Handles a request for this app.
-  function handleRequest(req, res, next, person) {
-    var content = {
-      analyticsScript: config.app.analyticsScript
-    };
-    res.render(templatePath, content);
+  function handleRequest(req, res, next) {
+    var dirs = req.path.split('/');
+
+    if (dirs[2] === '' || dirs.length === 2) {
+      res.redirect(301, 'http://levi.codes/');
+    } else {
+      next();
+    }
   }
 };
